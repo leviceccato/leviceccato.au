678b4e8ed6e820da4801d944cb7790b868842102
Levi Ceccato <levi.ceccato@gmail.com>
2023-06-30 08:32:28 +1000

    Add visuals and markup for Page without Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,70 +1,22 @@
-import { type ParentComponent } from 'solid-js'
-import { onMount } from 'solid-js'
+// Layout is a wrapper around Page to provide styling
+// and markup. This should be used at the root element
+// in route components.
 
-type HeadItem =
-	| [string, Record<string, string | boolean>]
-	| [string, Record<string, string | boolean>, string]
+import * as t from '#/utils/toolkit'
+import Page, { type PageProps } from '#/components/Page'
 
-type Props = {
-	title: string
-	description: string
-	head?: HeadItem[]
-}
-
-export default (function (props) {
-	const headCustom = () => props.head || []
-
-	const head = () => [
-		...headCustom(),
-		['title', {}, props.title],
-		['meta', { name: 'description', content: props.description }],
-	]
-
-	const headHtml = () => {
-		return head()
-			.map(([tag, attrs, content]) => {
-				let html = `<${tag} data-route-head `
-
-				Object.entries(attrs).forEach(([name, value]) => {
-					// Cast booleans to either non-existing or
-					// empty strings
-					if (value === false) {
-						return
-					}
-
-					if (value === true) {
-						value = ''
-					}
-
-					html += `${name}="${value}" `
-				})
-
-				if (!content) {
-					return `${html}/>`
-				}
-
-				return `${html}>${content}</${tag}>`
-			})
-			.join('')
-	}
-
-	onMount(() => {
-		// When the layout changes swap all old head elements with new ones
-		document.querySelectorAll('[data-route-head]').forEach((el) => el.remove())
-		document.head.insertAdjacentHTML('beforeend', headHtml())
-	})
-
-	return (
-		<>
-			{/* Serialise data so it can be added to head during prerendering */}
-			<script
-				type="application/json"
-				id="route-data"
-				textContent={JSON.stringify({
-					head: headHtml(),
-				})}
-			/>
-			<main>{props.children}</main>
-		</>
-	)
-} satisfies ParentComponent<Props>)
+export default t.component<PageProps>((props) => {
+    return (
+        <Page
+            title={props.title}
+            description={props.description}
+            head={props.head}
+            routeDataId="route-data"
+            headDataAttr="route-head"
+        >
+            <main>
+                {props.children}
+            </main>
+        </Page>
+    )
+})
