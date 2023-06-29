8c0069c8d5d819dc258dac09b9628c0f85bd80e2
Levi Ceccato <levi.ceccato@gmail.com>
2023-06-30 08:32:49 +1000

    Use Header in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,41 @@
-import { type Component, Suspense } from 'solid-js'
-import { useRoutes, A } from '@solidjs/router'
-import { routes } from '#/routes'
+import * as t from '#/utils/toolkit'
+import { Suspense } from 'solid-js'
+import { useRoutes } from '@solidjs/router'
+import { routes } from '#/utils/routes'
+import Header from '#/components/Header'
 
-export default (function () {
+const nav = [
+	[
+		{ text: 'Home', url: '/' },
+		{ text: 'Blog', url: '/blog' },
+	],
+	[
+		{ text: 'Logos', url: '/logos' },
+		{ text: 'Art', url: '/art' },
+	],
+	[
+		{ text: 'Colophon', url: '/colophon' },
+		{ text: 'Search', url: '/search' },
+	],
+]
+
+export default t.component(() => {
 	const Routes = useRoutes(routes)
 
 	return (
-		<div>
-			<header>
-				<A href="/">home</A>
-				<A href="/test">test</A>
-				<A href="/test/another">test/another</A>
-			</header>
+		<>
+			<Header
+				id="header"
+				endId="header-end"
+				nav={nav}
+			/>
+			{/*
+				Suspense is required for prerendering async
+				routes to work
+			*/}
 			<Suspense>
 				<Routes />
 			</Suspense>
-		</div>
+		</>
 	)
-} satisfies Component)
+})
