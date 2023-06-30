808ea205ed1c0b94f6e18aa27050660706802c08
Levi Ceccato <levi.ceccato@gmail.com>
2023-06-30 21:44:44 +1000

    Use Nav in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,40 @@
-import { type Component, For } from 'solid-js'
-import { A, useLocation } from '@solidjs/router'
+import { type Component } from 'solid-js'
+import { Nav } from '#/components/Nav'
 import * as css from './Header.css'
 
 export const Header: Component<{
 	id: string
 	// An destination for the skip link must be specified
 	skipLinkUrl: string
-	nav: { text: string; url: string }[][]
 }> = (props) => {
-	const location = useLocation()
-
 	return (
-		<header id={props.id}>
+		<header
+			id={props.id}
+			class={css.root}
+		>
 			<a
 				class="Skip VisuallyHidden focusable"
 				href={props.skipLinkUrl}
 			>
 				Skip to main content
 			</a>
-			<nav class={css.nav}>
-				<For each={props.nav}>
-					{(column) => (
-						<div class={css.column}>
-							<For each={column}>
-								{(item) => (
-									<A
-										class={css.link}
-										href={item.url}
-										textContent={item.text}
-										activeClass="active"
-										aria-current={item.url === location.pathname && 'page'}
-									/>
-								)}
-							</For>
-						</div>
-					)}
-				</For>
-			</nav>
+			<Nav
+				position="start"
+				columns={[
+					[
+						{ text: 'Home', url: '/' },
+						{ text: 'Blog', url: '/blog' },
+					],
+					[
+						{ text: 'Logos', url: '/logos' },
+						{ text: 'Art', url: '/art' },
+					],
+					[
+						{ text: 'Colophon', url: '/colophon' },
+						{ text: 'Search', url: '/search' },
+					],
+				]}
+			/>
 		</header>
 	)
 }
