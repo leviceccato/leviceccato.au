f9f95c0b2a55052d511a0ce109802937ce14468e
Levi Ceccato <levi.ceccato@gmail.com>
2023-06-30 19:34:15 +1000

    Add css functions

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,1 +1,21 @@
-export default {}
+import { breakpoints } from "#/data/breakpoints"
+
+export function rem(pixel: number): string {
+	return `${pixel / 16}rem`
+}
+
+export function clampVw(
+	minValue: number,
+	maxValue: number,
+	minVw = breakpoints[0],
+	maxVw = breakpoints[100],
+): string {
+	const slope = (minValue - maxValue) / (minVw - maxVw)
+	const intersection = maxValue - slope * maxVw
+	const _value = `${intersection}px + ${slope * 100}vw`
+	return `clamp(${minValue}px, ${_value}, ${maxValue}px)`
+}
+
+export function minBp(breakpoint: keyof typeof breakpoints): string {
+	return `(min-width: ${breakpoints[breakpoint]}px)`
+}
