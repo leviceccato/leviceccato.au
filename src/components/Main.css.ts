f12900b25d1706e0ecd0c616021ab9663378d6e1
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-01 19:30:27 +1000

    Add block padding to main

--- src/components/Main.css.ts
+++ src/components/Main.css.ts
@@ -1,5 +1,8 @@
 import { style } from '@vanilla-extract/css'
+import { clampVw } from '#/utils/misc'
 
 export const root = style({
 	flex: '1 0 auto',
+	paddingBlockStart: clampVw(35, 85),
+	paddingBlockEnd: clampVw(50, 175),
 })
