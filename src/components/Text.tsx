418a8e94a7450b71b5e3eaed9adfaf68ec362613
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-01 19:30:48 +1000

    Add Text

---
+++ src/components/Text.tsx
@@ -0,0 +1,6 @@
+import { type ParentComponent } from 'solid-js'
+import * as css from './Text.css'
+
+export const Text: ParentComponent = (props) => {
+	return <span class={css.root}>{props.children}</span>
+}
