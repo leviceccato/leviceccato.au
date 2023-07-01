995d021de847b1b97fbb36b44bd4efd961e420fe
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-01 12:10:05 +1000

    Remove unused function

--- src/toolkit.tsx
+++ src/toolkit.tsx
@@ -1,15 +1,10 @@
 import { type JSX, Component } from 'solid-js'
 import { type RouteProps } from '#/components/Page'
 import { Page } from '#/components/Page'
-import { type RouteMeta } from '#/data/routes'
 
 export function route(
 	props: RouteProps,
 	component: () => JSX.Element,
 ): Component {
 	return () => <Page {...props}>{component()}</Page>
-}
-
-export function meta(data: RouteMeta): RouteMeta {
-	return data
 }
