d4f5f543c04b2c496c0b9b3f6047b0db2a8580a4
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-01 10:18:37 +1000

    Expose route as object so metas can be accessed from path

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -4,54 +4,67 @@
 import { lazy, type Component } from 'solid-js'
 import { type RouteDefinition } from '@solidjs/router'
 
-const routeModules = import.meta.glob<{ default: Component<any> }>(
-	'../routes/**/*_*.tsx',
-)
-const routeMetas = import.meta.glob<RouteMeta | undefined>(
-	'../routes/**/*_*.tsx',
-	{
-		import: 'meta',
-		eager: true,
-	},
-)
-
 export type RouteMeta = {
 	isHidden?: boolean
 }
 
-const normalisedRouteModules = normaliseRoutePaths(routeModules)
-export const normalisedRouteMetas = normaliseRoutePaths(routeMetas)
-export const normalisedRoutes = Object.keys(normalisedRouteModules)
+// Route modules are imported lazily
+const routeModules = import.meta.glob<{ default: Component<any> }>(
+	'../routes/**/*_*.tsx',
+)
 
-export const routes = Object.entries(
-	normalisedRouteModules,
-).map<RouteDefinition>(([path, mod]) => ({ path, component: lazy(mod) }))
+// Route metas are imported statically (this will be treeshaken)
+const routeMetas = import.meta.glob<RouteMeta>('../routes/**/*_*.tsx', {
+	import: 'meta',
+	eager: true,
+})
 
-function normaliseRoutePaths<TValue>(modules: Record<string, TValue>) {
-	return Object.fromEntries(
-		Object.entries(modules)
-			.sort(([a], [b]) => a.localeCompare(b, 'en-AU', { numeric: true }))
-			.map<[string, TValue]>(([path, mod]) => {
-				let segments = path.split('/')
-
-				// Get segments relative to 'routes', e.g; ['about', '_index.tsx']
-				segments = segments.slice(
-					segments.indexOf('routes') + 1,
-					segments.length + 1,
-				)
-
-				// Remove number and or underscores
-				segments = segments.map((segment) => segment.split('_')[1])
-
-				const fileIndex = segments.length - 1
-				// Remove '.tsx'
-				segments[fileIndex] = segments[fileIndex].split('.')[0]
-				// Index files segments should be removed
-				if (segments[fileIndex] === 'index') {
-					segments.pop()
-				}
-
-				return ['/' + segments.join('/'), mod]
-			}),
-	) as Record<string, TValue>
+type Route = Pick<RouteDefinition, 'component' | 'path'> & {
+	meta: RouteMeta
+	filePath: string
 }
+
+// First transform into object so that metas can be accessed from a
+// provided route path
+export const routes = Object.entries(routeModules)
+	.sort(([a], [b]) => a.localeCompare(b, 'en-AU', { numeric: true }))
+	.map<Route>(([path, mod]) => {
+		let segments = path.split('/')
+
+		// Get segments relative to 'routes', e.g; ['about', '_index.tsx']
+		segments = segments.slice(
+			segments.indexOf('routes') + 1,
+			segments.length + 1,
+		)
+
+		// Remove number and or underscores
+		segments = segments.map((segment) => segment.split('_')[1])
+
+		const fileIndex = segments.length - 1
+		// Remove '.tsx'
+		segments[fileIndex] = segments[fileIndex].split('.')[0]
+		// Index files segments should be removed
+		if (segments[fileIndex] === 'index') {
+			segments.pop()
+		}
+
+		return {
+			filePath: path,
+			meta: routeMetas[path],
+			path: '/' + segments.join('/'),
+			component: lazy(mod),
+		}
+	})
+	.reduce<Record<string, Omit<Route, 'path'>>>((previous, current) => {
+		previous[current.path] = {
+			meta: current.meta,
+			filePath: current.filePath,
+			component: current.component,
+		}
+		return previous
+	}, {})
+
+// For use in Solid Router and our prerender script
+export const appRoutes = Object.entries(routes).map<RouteDefinition>(
+	([path, route]) => ({ path, component: route.component }),
+)
