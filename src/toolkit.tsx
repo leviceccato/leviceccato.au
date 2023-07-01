f1929bb8459301d8d94deaf3ba7ae63dea11d25d
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-01 19:30:42 +1000

    Expose new components in toolkit

--- src/toolkit.tsx
+++ src/toolkit.tsx
@@ -2,6 +2,10 @@ import { type JSX, Component } from 'solid-js'
 import { type RouteProps } from '#/components/Page'
 import { Page } from '#/components/Page'
 
+export { H1 } from '#/components/H1'
+export { A } from '#/components/A'
+export { P } from '#/components/P'
+
 export function route(
 	props: RouteProps,
 	component: () => JSX.Element,
