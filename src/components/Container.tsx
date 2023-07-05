f61041d362e0fb3e085006504136ec192a2a0110
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-05 20:24:53 +1000

    Allow Container to accept a class

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,10 @@
-import { type ParentComponent } from 'solid-js'
+import { type ParentComponent, mergeProps } from 'solid-js'
 import * as css from './Container.css'
 
-export const Container: ParentComponent = (props) => {
-	return <div class={css.root}>{props.children}</div>
+export const Container: ParentComponent<{
+	class?: string
+}> = (props) => {
+	const _props = mergeProps({ class: '' }, props)
+
+	return <div class={`${css.root} ${_props.class}`}>{_props.children}</div>
 }
