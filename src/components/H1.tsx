5127b1731c47ae14c7592b3e8727a935736d19af
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-05 19:54:14 +1000

    Use Text in H1

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,11 +1,19 @@
 import { type ParentComponent } from 'solid-js'
 import { Container } from '#/components/Container'
+import { Text } from '#/components/Text'
 import * as css from './H1.css'
 
 export const H1: ParentComponent = (props) => {
 	return (
 		<Container>
-			<h1 class={css.root}>{props.children}</h1>
+			<h1 class={css.root}>
+				<Text
+					lineHeight={1.15}
+					variant="block"
+				>
+					{props.children}
+				</Text>
+			</h1>
 		</Container>
 	)
 }
