88fd60bac450f1057911df8dbf3374b3940ee0d3
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-05 20:25:02 +1000

    Use Heading in H1

--- src/components/H1.css.ts
+++ src/components/H1.css.ts
@@ -2,10 +2,7 @@ import { style } from '@vanilla-extract/css'
 import { clampVw } from '#/utils/misc'
 
 export const root = style({
-	display: 'block',
-	fontWeight: 700,
 	letterSpacing: '-0.03em',
 	lineHeight: 1.15,
 	fontSize: clampVw(33, 47),
-	// @include lh-crop(1.15);
 })
--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,19 +1,14 @@
 import { type ParentComponent } from 'solid-js'
-import { Container } from '#/components/Container'
-import { Text } from '#/components/Text'
+import { Heading } from '#/components/Heading'
 import * as css from './H1.css'
 
 export const H1: ParentComponent = (props) => {
 	return (
-		<Container>
-			<h1 class={css.root}>
-				<Text
-					lineHeight={1.15}
-					variant="block"
-				>
-					{props.children}
-				</Text>
-			</h1>
-		</Container>
+		<Heading
+			level="1"
+			class={css.root}
+		>
+			{props.children}
+		</Heading>
 	)
 }
