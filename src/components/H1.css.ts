2672f6be56946df943336151aa56f99149aafd81
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-05 21:19:46 +1000

    Properly define line height

--- src/components/H1.css.ts
+++ src/components/H1.css.ts
@@ -1,8 +1,11 @@
 import { style } from '@vanilla-extract/css'
 import { clampVw } from '#/utils/misc'
+import { lineHeightVar } from '#/components/Text.css'
 
 export const root = style({
 	letterSpacing: '-0.03em',
-	lineHeight: 1.15,
 	fontSize: clampVw(33, 47),
+	vars: {
+		[lineHeightVar]: '1.15',
+	},
 })
