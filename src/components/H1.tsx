d99e23c9c2f9114054d273c20075796ab79def97
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 07:30:29 +1000

    Define underline vars statically

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,10 +1,5 @@
 import { type ParentComponent } from 'solid-js'
 import { Heading } from '#/components/Heading'
-import { assignInlineVars } from '@vanilla-extract/dynamic'
-import {
-	textDecorationThicknessVar,
-	textUnderlineOffsetVar,
-} from '#/components/A.css'
 import * as css from './H1.css'
 
 export const H1: ParentComponent = (props) => {
@@ -12,10 +7,6 @@ export const H1: ParentComponent = (props) => {
 		<Heading
 			level="1"
 			class={css.root}
-			style={assignInlineVars({
-				[textDecorationThicknessVar]: '3px',
-				[textUnderlineOffsetVar]: '4px',
-			})}
 		>
 			{props.children}
 		</Heading>
