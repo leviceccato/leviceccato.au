d6d70efa3f1929ba2eea10c8d43eeaaa9b91715a
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-11 20:56:54 +1000

    Provide default A underline styles for H1

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,5 +1,10 @@
 import { type ParentComponent } from 'solid-js'
 import { Heading } from '#/components/Heading'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
+import {
+	textDecorationThicknessVar,
+	textUnderlineOffsetVar,
+} from '#/components/A.css'
 import * as css from './H1.css'
 
 export const H1: ParentComponent = (props) => {
@@ -7,6 +12,10 @@ export const H1: ParentComponent = (props) => {
 		<Heading
 			level="1"
 			class={css.root}
+			style={assignInlineVars({
+				[textDecorationThicknessVar]: '3px',
+				[textUnderlineOffsetVar]: '4px',
+			})}
 		>
 			{props.children}
 		</Heading>
