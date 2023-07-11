004f6bf85caa6fb5a75f75a4dc087519c30575b9
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 07:26:00 +1000

    Add default margins

--- src/components/H1.css.ts
+++ src/components/H1.css.ts
@@ -4,6 +4,8 @@ import { lineHeightVar } from '#/components/Text.css'
 
 export const root = style({
 	letterSpacing: '-0.03em',
+	marginBlockStart: '3em',
+	marginBlockEnd: '0.9em',
 	fontSize: clampVw(33, 47),
 	vars: {
 		[lineHeightVar]: '1.15',
