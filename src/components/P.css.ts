e4e8a4b03c44f405e223c45717c73be749a983fa
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 10:52:16 +1000

    Show underlines for A's in P by default

--- src/components/P.css.ts
+++ src/components/P.css.ts
@@ -1,7 +1,11 @@
 import { style } from '@vanilla-extract/css'
 import { clampVw } from '#/utils/misc'
+import { defaultTextDecorationLineVar } from '#/components/A.css'
 
 export const paragraph = style({
 	lineHeight: 1.5,
 	fontSize: clampVw(16, 17),
+	vars: {
+		[defaultTextDecorationLineVar]: 'underline',
+	},
 })
