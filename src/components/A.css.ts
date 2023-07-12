f03158d2c5bae53e6009e4d6f76427a105c03111
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 11:14:03 +1000

    Allow overriding hover stroke and font weight

--- src/components/A.css.ts
+++ src/components/A.css.ts
@@ -3,9 +3,11 @@ import { style, createVar, fallbackVar } from '@vanilla-extract/css'
 export const textDecorationThicknessVar = createVar()
 export const textUnderlineOffsetVar = createVar()
 export const defaultTextDecorationLineVar = createVar()
+export const fontWeightVar = createVar()
+export const hoverStrokeVar = createVar()
 
 export const root = style({
-	fontWeight: 700,
+	fontWeight: fallbackVar(fontWeightVar, '700'),
 	letterSpacing: '-0.02em',
 })
 
@@ -13,10 +15,11 @@ export const main = style({
 	pointerEvents: 'none',
 	textDecorationThickness: fallbackVar(textDecorationThicknessVar, '0.09em'),
 	textDecorationLine: fallbackVar(defaultTextDecorationLineVar, 'none'),
+	textUnderlineOffset: fallbackVar(textUnderlineOffsetVar, '0.1em'),
 	selectors: {
 		[`${root}:hover &`]: {
 			textDecorationLine: 'underline',
-			textUnderlineOffset: fallbackVar(textUnderlineOffsetVar, '0.1em'),
+			WebkitTextStroke: fallbackVar(hoverStrokeVar, '0 currentColor'),
 		},
 	},
 })
