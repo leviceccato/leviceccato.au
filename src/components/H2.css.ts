318b9713d8c41f644d77a2d8ac6b868cbbedc1e3
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 18:18:18 +1000

    Fix H2 underline styles

--- src/components/H2.css.ts
+++ src/components/H2.css.ts
@@ -1,6 +1,10 @@
 import { style } from '@vanilla-extract/css'
 import { clampVw } from '#/utils/misc'
 import { lineHeightVar } from '#/components/Text.css'
+import {
+	textDecorationThicknessVar,
+	textUnderlineOffsetVar,
+} from '#/components/A.css'
 
 export const root = style({
 	letterSpacing: '-0.03em',
@@ -9,5 +13,7 @@ export const root = style({
 	fontSize: clampVw(33, 47),
 	vars: {
 		[lineHeightVar]: '1.15',
+		[textDecorationThicknessVar]: '3px',
+		[textUnderlineOffsetVar]: '4px',
 	},
 })
