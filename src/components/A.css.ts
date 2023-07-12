919d2fdaedacae76d6bb9ec2f2485d537ba55bf6
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 11:21:54 +1000

    Allow overriding more A styles

--- src/components/A.css.ts
+++ src/components/A.css.ts
@@ -1,10 +1,13 @@
 import { style, createVar, fallbackVar } from '@vanilla-extract/css'
 
 export const textDecorationThicknessVar = createVar()
+export const hoverTextDecorationThicknessVar = createVar()
 export const textUnderlineOffsetVar = createVar()
 export const defaultTextDecorationLineVar = createVar()
 export const fontWeightVar = createVar()
 export const hoverStrokeVar = createVar()
+
+export const defaultTextDecorationThickness = '0.09em'
 
 export const root = style({
 	fontWeight: fallbackVar(fontWeightVar, '700'),
@@ -13,13 +16,21 @@ export const root = style({
 
 export const main = style({
 	pointerEvents: 'none',
-	textDecorationThickness: fallbackVar(textDecorationThicknessVar, '0.09em'),
+	textDecorationThickness: fallbackVar(
+		textDecorationThicknessVar,
+		defaultTextDecorationThickness,
+	),
 	textDecorationLine: fallbackVar(defaultTextDecorationLineVar, 'none'),
 	textUnderlineOffset: fallbackVar(textUnderlineOffsetVar, '0.1em'),
 	selectors: {
 		[`${root}:hover &`]: {
 			textDecorationLine: 'underline',
 			WebkitTextStroke: fallbackVar(hoverStrokeVar, '0 currentColor'),
+			textDecorationThickness: fallbackVar(
+				hoverTextDecorationThicknessVar,
+				textDecorationThicknessVar,
+				defaultTextDecorationThickness,
+			),
 		},
 	},
 })
