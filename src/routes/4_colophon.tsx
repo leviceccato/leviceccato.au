b9757e2776fceabc1fa144ebd178e05cfce93326
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-12 20:07:21 +1000

    Add colophon text

--- src/routes/4_colophon.tsx
+++ src/routes/4_colophon.tsx
@@ -7,7 +7,18 @@ export default t.route(
 	},
 	() => (
 		<>
-			<div>Colophon</div>
+			<t.H1>Colophon</t.H1>
+			<t.P>
+				Created in July 2023 by Levi Ceccato and continually updated. All
+				content is subject to copyright.
+			</t.P>
+			<t.H2>Technical</t.H2>
+			<t.P>
+				This site is built as a SolidJS SPA with prerendering and is hosted on
+				Netlify. The project is open source and on GitHub.
+			</t.P>
+			<t.H2>Typography</t.H2>
+			<t.P>Typeset in Manrope by Mikhail Sharanda.</t.P>
 		</>
 	),
 )
