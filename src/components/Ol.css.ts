88bed9b22ba2aa9645a7d6b6f6dc68fdc77f6f4a
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-15 15:23:42 +1000

    Add spacing to list items

--- src/components/Ol.css.ts
+++ src/components/Ol.css.ts
@@ -1,5 +1,9 @@
 import { style } from '@vanilla-extract/css'
+import { clampVw } from '#/utils/misc'
 
 export const root = style({
 	counterReset: 'counter',
+	display: 'flex',
+	flexDirection: 'column',
+	gap: clampVw(7, 15),
 })
--- src/components/Ul.css.ts
+++ src/components/Ul.css.ts
@@ -1,3 +1,8 @@
 import { style } from '@vanilla-extract/css'
+import { clampVw } from '#/utils/misc'
 
-export const root = style({})
+export const root = style({
+	display: 'flex',
+	flexDirection: 'column',
+	gap: clampVw(7, 15),
+})
