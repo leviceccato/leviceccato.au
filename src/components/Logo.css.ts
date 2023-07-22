173aeff4381102defa8922e4fea930b8f3d5965d
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 09:45:10 +1000

    Add WIP logo page and logos

--- src/components/Logo.css.ts
+++ src/components/Logo.css.ts
@@ -2,5 +2,6 @@ import { style } from '@vanilla-extract/css'
 import { colours } from '#/data/theme'
 
 export const root = style({
-	backgroundColor: colours.bg,
+	backgroundColor: colours.fg,
+	marginBottom: 2,
 })
--- src/routes/2_logos/_index.tsx
+++ src/routes/2_logos/_index.tsx
@@ -1,4 +1,16 @@
 import * as t from '#/toolkit'
+import getSmartLogo from './get-smart.svg?raw'
+import brookesLogo from './brookes.svg?raw'
+import rxEliteLogo from './rx-elite.svg?raw'
+import arcLogo from './arc.svg?raw'
+import rubixLogo from './rubix.svg?raw'
+import addMyLogoLogo from './add-my-logo.svg?raw'
+import diceRollerLogo from './dice-roller.svg?raw'
+import cityOfNewcastleLogo from './city-of-newcastle.svg?raw'
+import newcastleFringeLogo from './newcastle-fringe.svg?raw'
+import walkOfTheWanderingManLogo from './walk-of-the-wandering-man.svg?raw'
+import arcadiaLogo from './arcadia.svg?raw'
+import designWithDevLogo from './design-with-dev.svg?raw'
 
 export const meta = {
 	title: 'Logos',
@@ -8,6 +20,57 @@ export const meta = {
 export default t.route(meta, () => (
 	<>
 		<t.H1>Logos</t.H1>
-		<t.P>Logos</t.P>
+		<t.P>
+			A collection of logos and marks for various commerical and non-commercial
+			projects.
+		</t.P>
+		<t.Logo
+			svg={getSmartLogo}
+			padding="30%"
+		/>
+		<t.Logo
+			svg={brookesLogo}
+			padding="30%"
+		/>
+		<t.Logo
+			svg={rxEliteLogo}
+			padding="35%"
+		/>
+		<t.Logo
+			svg={arcLogo}
+			padding="38%"
+		/>
+		<t.Logo
+			svg={rubixLogo}
+			padding="35%"
+		/>
+		<t.Logo
+			svg={addMyLogoLogo}
+			padding="35%"
+		/>
+		<t.Logo
+			svg={diceRollerLogo}
+			padding="27%"
+		/>
+		<t.Logo
+			svg={cityOfNewcastleLogo}
+			padding="27%"
+		/>
+		<t.Logo
+			svg={newcastleFringeLogo}
+			padding="27%"
+		/>
+		<t.Logo
+			svg={walkOfTheWanderingManLogo}
+			padding="25%"
+		/>
+		<t.Logo
+			svg={arcadiaLogo}
+			padding="32%"
+		/>
+		<t.Logo
+			svg={designWithDevLogo}
+			padding="28%"
+		/>
 	</>
 ))
