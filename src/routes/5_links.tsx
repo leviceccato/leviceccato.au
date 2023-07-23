13bb71e8f10bb2049b9439bd31cdc492915676d3
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 12:13:33 +1000

    Add links page copy

--- src/routes/5_links.tsx
+++ src/routes/5_links.tsx
@@ -8,6 +8,8 @@ export const meta = {
 export default t.route(meta, () => (
 	<>
 		<t.H1>Links</t.H1>
-		<t.P>Links</t.P>
+		<t.P>
+			Here a few different places you can find me throughout the internet.
+		</t.P>
 	</>
 ))
