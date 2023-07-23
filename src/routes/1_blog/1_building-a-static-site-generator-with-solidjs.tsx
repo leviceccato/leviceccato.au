0bd0205e4d0b6f863f477b07125316ca5e2977f3
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 16:39:10 +1000

    Update wording

--- src/routes/1_blog/1_building-a-static-site-generator-with-solidjs.tsx
+++ src/routes/1_blog/1_building-a-static-site-generator-with-solidjs.tsx
@@ -9,9 +9,8 @@ export default t.route(meta, () => (
 	<>
 		<t.H1>Building a static site generator with SolidJS</t.H1>
 		<t.P>
-			As a preface this is mostly just an excuse to write about my process and
-			findings in building this site (my site). An honourary and obligatory
-			first post.
+			This is mostly just an excuse to write about my processes and findings in
+			building this site (my site). An honourary and obligatory first post.
 		</t.P>
 	</>
 ))
