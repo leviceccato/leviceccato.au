3ab348cf3926f14995359487f285f53d8f833432
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 16:25:15 +1000

    Add links

--- src/routes/4_colophon.tsx
+++ src/routes/4_colophon.tsx
@@ -14,12 +14,18 @@ export default t.route(meta, () => (
 		</t.P>
 		<t.H2>Technical</t.H2>
 		<t.P>
-			This site is built as a SolidJS SPA with full prerendering, essentially
-			making it a static site generator. As a result, it works without
-			JavaScript, but once hydrated, dynamic functionality becomes available.
-			It's hosted on Netlify and the source is available on GitHub.
+			This site is built as a <t.A href="https://www.solidjs.com">SolidJS</t.A>{' '}
+			SPA with full prerendering, essentially making it a static site generator.
+			As a result, it works without JavaScript, but once hydrated, dynamic
+			functionality becomes available. It's hosted on{' '}
+			<t.A href="https://www.netlify.com">Netlify</t.A> and the source is
+			available on{' '}
+			<t.A href="https://github.com/leviceccato/leviceccato.au">GitHub</t.A>.
 		</t.P>
 		<t.H2>Typography</t.H2>
-		<t.P>Typeset in Manrope by Mikhail Sharanda.</t.P>
+		<t.P>
+			Typeset in <t.A href="https://www.gent.media/manrope">Manrope</t.A> by
+			<t.A href="https://www.gent.media">Mikhail Sharanda</t.A>.
+		</t.P>
 	</>
 ))
