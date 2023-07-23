a8a719a1b0ddec33713d2d19a2e8a13c0180d3f2
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 16:25:34 +1000

    Add WIP first post

---
+++ src/routes/1_blog/1_building-a-static-site-generator-with-solidjs.tsx
@@ -0,0 +1,17 @@
+import * as t from '#/toolkit'
+
+export const meta = {
+	title: 'Building a static site generator with SolidJS',
+	description: 'asd',
+}
+
+export default t.route(meta, () => (
+	<>
+		<t.H1>Building a static site generator with SolidJS</t.H1>
+		<t.P>
+			As a preface this is mostly just an excuse to write about my process and
+			findings in building this site (my site). An honourary and obligatory
+			first post.
+		</t.P>
+	</>
+))
