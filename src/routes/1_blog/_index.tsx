ea3b9c535e6d86efbc34af398a8e36a3eb98b5dc
Levi Ceccato <levi.ceccato@gmail.com>
2023-07-23 16:25:28 +1000

    Work on blog copy a bit more

--- src/routes/1_blog/_index.tsx
+++ src/routes/1_blog/_index.tsx
@@ -2,12 +2,13 @@ import * as t from '#/toolkit'
 
 export const meta = {
 	title: 'Blog',
-	description: 'asd',
+	description: 'Thoughts at the intersection of design and development.',
 }
 
 export default t.route(meta, () => (
 	<>
 		<t.H1>Blog</t.H1>
-		<t.P>Blog</t.P>
+		<t.P>Thoughts at the intersection of design and development.</t.P>
+		<t.Listing />
 	</>
 ))
