99bf0f863867a99d6bdd63c8cf0d551505de035a
Levi Ceccato <levi.ceccato@gmail.com>
2023-09-03 18:06:44 +1000

    Make more readable

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -43,7 +43,11 @@ export function slugify(text: string): string {
 
 // Format Date to string for use in blog
 export function formatDate(date: Date): string {
-	return `${date.getUTCDay()} ${new Intl.DateTimeFormat('en-AU', {
+	const day = date.getUTCDay()
+	const month = new Intl.DateTimeFormat('en-AU', {
 		month: 'short',
-	}).format(date)} ${date.getUTCFullYear()}`
+	}).format(date)
+	const year = date.getUTCFullYear()
+
+	return `${day} ${month} ${year}`
 }
