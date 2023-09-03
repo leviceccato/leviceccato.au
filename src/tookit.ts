a491462e8b116020440fa2d04110270d923d2f44
Levi Ceccato <levi.ceccato@gmail.com>
2023-09-03 18:03:43 +1000

    Make ts ignoring method more robust and document

--- src/tookit.ts
+++ src/tookit.ts
@@ -2,19 +2,35 @@
 // for use in pages. No check so we can re-export Astro
 // components
 
-// @ts-nocheck
+// The `ts-ignore` comments here are necessary because we're
+// importing this file inside TypeScript, which means that
+// `tsc` will try to resolve these imports.
 
+// @ts-ignore
 export { default as Layout } from './components/Layout.astro'
+// @ts-ignore
 export { default as Container } from './components/Container.astro'
+// @ts-ignore
 export { default as Text } from './components/Text.astro'
+// @ts-ignore
 export { default as A } from './components/A.astro'
+// @ts-ignore
 export { default as P } from './components/P.astro'
+// @ts-ignore
 export { default as H1 } from './components/H1.astro'
+// @ts-ignore
 export { default as H2 } from './components/H2.astro'
+// @ts-ignore
 export { default as H3 } from './components/H3.astro'
+// @ts-ignore
 export { default as Ol } from './components/Ol.astro'
+// @ts-ignore
 export { default as Oli } from './components/Oli.astro'
+// @ts-ignore
 export { default as Ul } from './components/Ul.astro'
+// @ts-ignore
 export { default as Uli } from './components/Uli.astro'
+// @ts-ignore
 export { default as Quote } from './components/Quote.astro'
+// @ts-ignore
 export { default as Figure } from './components/Figure.astro'
