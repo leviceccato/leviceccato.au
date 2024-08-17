c5d3028703e3f5bccd4d35ad944cb087e865abf1
Levi Ceccato <levi.ceccato@gmail.com>
2024-08-17 18:02:31 +1000

    nvm I don't like this

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 export default {
+	root: 'src',
 	clearScreen: false,
 	server: {
 		host: true,
+	},
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true,
 	},
 	css: {
 		transformer: 'lightningcss',
