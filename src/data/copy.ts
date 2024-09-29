93bfbc206e90ec518f75bf37f56dd958365d369c
Levi Ceccato <levi.ceccato@gmail.com>
2024-09-29 14:20:38 +1000

    Typo

--- src/data/copy.ts
+++ src/data/copy.ts
@@ -1,4 +1,4 @@
 export const title = 'Levi Ceccato'
 
 export const description =
-	'I’m a multidisciplinary designer based in Townsville‚ Australia. My passion for design drives me to explore all of it’s avenues‚ including illustration‚ graphic design and software development. I’m currently developing'
+	'I’m a multidisciplinary designer based in Townsville‚ Australia. My passion for design drives me to explore all of it’s avenues‚ including illustration‚ graphic design and software development.'
