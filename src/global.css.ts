2f158dc82bd70e6be922bb816d4f30e9a5494f55
Levi Ceccato <levi.ceccato@gmail.com>
2024-09-29 12:04:55 +1000

    Avoid reset, reset at component level instead

--- src/global.css.ts
+++ src/global.css.ts
@@ -25,18 +25,31 @@ export const fontInterDisplayRegular = {
 	}),
 } satisfies Font
 
+/**
+ * Rather than include a CSS reset, this project resets styles at the component
+ * level. Any reset properties are likely taken from modern-normalize.
+ */
+
 g('html', {
 	/**
 	 * By setting the root font size to this value we can use 1rem, 2rem,
 	 * 3rem etc to represent 10px, 20px, 30px respectively.
 	 */
 	fontSize: '0.625rem',
+	/**
+	 * Prevent adjustments of font size after orientation changes in iOS.
+	 */
+	WebkitTextSizeAdjust: '100%',
 })
 
 g('body', {
+	margin: 0,
 	backgroundColor: '',
 	color: '',
 	blockSize: '200vh',
+	/**
+	 * Improve text rendering in all browsers.
+	 */
 	WebkitFontSmoothing: 'antialiased',
 	MozOsxFontSmoothing: 'grayscale',
 })
