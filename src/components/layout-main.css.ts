3a70cb2d4a310083d49a1c97bb15ec395225ac0c
Levi Ceccato <levi.ceccato@gmail.com>
2024-09-29 14:20:45 +1000

    Add nav style

--- src/components/layout-main.css.ts
+++ src/components/layout-main.css.ts
@@ -1,6 +1,12 @@
 import { style } from '@vanilla-extract/css'
 
-export const nav = style({})
+export const nav = style({
+	position: 'fixed',
+	insetInlineStart: '2rem',
+	insetBlockStart: '2rem',
+	display: 'flex',
+	gap: '2rem',
+})
 
 export const link = style({})
 
